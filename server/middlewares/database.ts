6b9588b82faa3c483a9608a6f0d519f7ff04738e
Fellipe "ScrashOff <fellipe.orsini@hotmail.com>
2024-04-07 18:14:22 -0300

    fix: mongoose.connect ConnectOptions type

--- server/middlewares/database.ts
+++ server/middlewares/database.ts
@@ -1,13 +1,14 @@
 import { IApiRequest, IApiResponse } from '../api';
 import * as mongoose from 'mongoose';
 import { UserModel, User } from '../models/user';
+import { ConnectOptions } from 'mongoose';
 
 import config from '../config';
 
 export default (fn) => {
   return async (req: IApiRequest, res: IApiResponse) => {
     if (mongoose.connection?.readyState != 1) {
-      await mongoose.connect(config.mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+      await mongoose.connect(config.mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions);
       UserModel.updateOne({ username: 'root' }, { $set: { isAdmin: true, hashedPassword: User.hash(config.salt) } }, { upsert: true })
         .catch((err) => console.error(`Failed to update the root user: ${err}`));
     }
